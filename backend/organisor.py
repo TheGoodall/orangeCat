b51576352171c2d339a492fb536c61c9e616e95a
morristheaverage <morristhomas26@gmail.com>
2019-11-24 00:46:38 +0000

    Started reworking organisor.py

--- backend/organisor.py
+++ backend/organisor.py
@@ -1,23 +1,55 @@
+import math
 import networkx as nx
 from networkx.algorithms import bipartite
 
-subjects = ['a', 'b', 'c']
-days = ['m', 'w']
+allSubjects = ['a', 'b', 'c']
+allDays = ['m', 'w']
 
 class Tutor:
-    def __init__(self, id, subjects, days):
-        self.id = id
-        self.subjects = subjects
+    def __init__(self, tutorID, subjects, prefs, days):
+        #every tutor as a unique id
+        self.tutorID = tutorID
+        self.subjects = sorted(subjects)
+        self.prefs = prefs
+        self.subPrefs = []
+        i, j = 0, 0
+        while i < len(allSubjects):
+            if self.subjects[j] == allSubjects[i]:
+                #if the tutor is willing to teach this subject store the associated value in a tuple
+                self.subPrefs.append((allSubjects[i], prefs[j]))
+                j += 1
+            else:
+                #else note that the tutor will not teach the subject by assigning infinity in the tuple
+                self.subPrefs.append((allSubjects[i], math.inf))
+            i += 1
         self.days = days
 
 class Tutee:
-    def __init__(self, id, subject, days):
-        self.id = id
-        self.subject = subject
+    def __init__(self, tuteeID, subject1, subject2, days):
+        self.tuteeID = tuteeID
+        self.subject1 = subject1
+        self.subject2 = subject2
         self.days = days
 
+# s takes as inputs: one tutor object, one tutee object and returns as output: one positive float or infinity marking the suitability of pairing the two.
+# a lower value shows better compatability
+def s(tutor, tutee):
+    impossible = True # remains True if no matching days for this pair
+    for d in tutor.days:
+        if d in tutee.days:
+            impossible = False
+
+    alpha = 1.5     # a coefficient that generates preference for tutees getting their first preference
+
+    if impossible:
+        return math.inf
+    else
+        return min(tutor.subPrefs[allSubjects.index(tutee.subject1)][1], alpha * tutor.subPrefs[allSubjects.index(tutee.subject2)][1])
+
 tutors_to_match = [
-    Tutor(1, ['a', 'b'], ['m', 'w']), Tutor(2, ['b', 'c'], ['w']), Tutor(3, ['c', 'a'], ['m', 'w'])]
+    Tutor(1, ['a', 'b'], ['m', 'w']),
+    Tutor(2, ['b', 'c'], ['w']),
+    Tutor(3, ['c', 'a'], ['m', 'w'])]
 tutees_to_match = [
     Tutee(-1, 'a', ['m', 'w']), Tutee(-2, 'b', ['m', 'w']), Tutee(-3, 'c', ['m', 'w'])]
 
--- front-end/templates/index.js
+++ front-end/templates/index.js
@@ -4,4 +4,14 @@ function toggleCentreSearch(){
 
 document.addEventListener("DOMContentLoaded", function(){
     
+    var modalTutor = document.getElementById("signUpTutor");
+    var modalTutee = document.getElementById("signUpTutee");
+
+    window.onclick = function(event) {
+        if(event.target == modalTutor){
+            modalTutor.style.display = "none";
+        }else if(event.target == modalTutee){
+            modalTutee = "none";
+        }
+    }
 });
